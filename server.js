206e01319e7fc51cdc56bcdf499c264dfe1c7702
Daniel Hong <daniellimhong@gmail.com>
2020-06-02 04:09:00 -0700

    fixed heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require('./app');
 
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT;
 if (port == null || port == "") port = 8000;
 
 app.listen(port, console.log(`Server listening on port ${port}`));
