15d7bc7a7d7f69c918fdaa6a32b5f9f1913efab8
Daniel Hong <daniellimhong@gmail.com>
2020-06-02 04:25:05 -0700

    fixed bug with port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const app = require('./app');
 
-const port = process.env.PORT;
-if (port == null || port == "") port = 8000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
 
 app.listen(port, console.log(`Server listening on port ${port}`));
